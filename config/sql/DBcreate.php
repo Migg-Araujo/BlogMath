dccf0f1e51b839b70a4d4b004779388e3aa2c084
Migg Araujo <miguelbzr6@gmail.com>
2024-01-02 20:32:46 -0300

    feat: edit sql database

--- config/sql/DBcreate.php
+++ config/sql/DBcreate.php
@@ -23,9 +23,10 @@
     if ($sql_query === TRUE) {
 
     }
-
-    $sql = 'CREATE TABLE IF NOT EXISTS POST(
-                ID_POST INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+    
+    $sql = 'CREATE TABLE IF NOT EXISTS CATEGORY(
+                ID_CATEGORY INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                TITLE VARCHAR(60)
             )';
     $sql_query = $conn->query($sql);
 
@@ -33,8 +34,27 @@
         
     }
 
-    $sql = 'CREATE TABLE IF NOT EXISTS CATEGORY(
-                ID_CATEGORY INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+    $sql = 'CREATE TABLE IF NOT EXISTS POST(
+                ID_POST INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                TITLE_REAL VARCHAR(70),
+                TITLE_SEARCH VARCHAR(70),
+                LIKES INT,
+                VIEWS INT,
+                DESC VARCHAR(255),
+                TEXT_POST VARCHAR(2000),
+                IMG VARCHAR(255)
+            )';
+    $sql_query = $conn->query($sql);
+
+    if ($sql_query === TRUE) {
+        
+    }
+
+    $sql = 'CREATE TABLE IF NOT EXISTS POSTCATEGORY(
+                ID_POST INT NOT NULL,
+                ID_CATEGORY INT NOT NULL,
+                FOREIGN KEY(ID_POST) REFERENCES POST(ID_POST),
+                FOREIGN KEY(ID_CATEGORY) REFERENCES CATEGORY(ID_CATEGORY)
             )';
     $sql_query = $conn->query($sql);
 
