e15a6f69e0236895bcf698431fde3fec9de3a9df
Miguel Araújo <miguelbzr6@gmail.com>
2024-01-20 12:48:23 -0300

    feat: add user page

--- pages/user.php
+++ pages/user.php
@@ -9,7 +9,24 @@ if(!isset($_SESSION['user'])){
 </script>
 <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>css/user.css">
 <main>
-    <div class='container'></div>
-    <div class='container'></div>
+    <div class='container'>
+        <h1>Your Profile</h1>
+        <img src="<?php echo INCLUDE_PATH; ?>/src/user-white.svg"/>
+        <p>Name: Lorem Ipsum</p>
+        <p>Username: Lorem</p>
+        <p>Email: Lorem Ipsum@gmail.com</p>
+
+        <button onclick="window.location.href='<?php echo INCLUDE_PATH;?>profile'">EDIT</button>
+    </div>
+    <div class='container'>
+    <h1>Your Post</h1>
+    
+        <p>Post: 0</p>
+        <p>Views: 0</p>
+        <p>Likes: 0</p>
+        <p>Followers: 0</p>
+
+        <button>POSTS</button>
+    </div>
 </main>
 <?php include 'footer.php';?>
